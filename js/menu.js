9d25d0655dda655157747d4c6327a945f1de68ff
wolfvolkov <kirillwolfvolkov@mail.ru>
2012-12-21 22:47:03 +0200

    b

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var menu1 = document.getElementById('menu1');
+var content ="";
+
+content = '<div id="menu" align="center"><a href="#M_game_area"><div id="bPlay" class="M_button" align="center" onClick="Start()" > Play</div></a><div id="bSeting" class="M_button" >Setings</div><div id="bHelp" class="M_button" >Help</div></div>';
+
+
+menu1.innerHTML=content;
+//onClick="PauseG()"
